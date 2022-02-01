e8de74ef15212fef01aa18ffdc986edcdf675a3e
YuriyPimenov <fenics1991@mail.ru>
2022-02-01 19:17:30 +0500

    init

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import styles from './menu.module.scss';
+
+const Menu: FC = () => {
+    return (
+        <div className={styles.root}>
+            Menu
+        </div>
+    );
+};
+
+export {Menu};
